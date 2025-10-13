7d40557fc9975ee1b62a0749c11095dedd047345
Tobi Akere <tobiakere25@gmail.com>
2025-10-12 20:51:58 -0500

    Refactored web view for hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -7,43 +7,44 @@ interface SectionProps {
 
 export function HeroSection({ onContinue }: SectionProps) {
   return (
-    <div className="flex flex-col items-center h-full w-full">
-      {/* Title and Description */}
-      <Fade direction="up" duration={1000}>
-        <section className="h-fit mt-20 flex flex-col text-center">
-          <h1 className="text-4xl font-bold mb-2">
-            MAV<span className="font-extralight">RESUME</span>
-          </h1>
-          <p className="text-lg">
-            Helping undergraduates quickly build tailored, professional resumes
-            - simple, guided, and stress-free.
-          </p>
-        </section>
-      </Fade>
-      {/* Action Buttons */}
-      <Fade direction="up" delay={400} duration={1000}>
-        <section className="flex gap-5 mt-3 ">
-          <button
-            onClick={onContinue}
-            className="btn font-bold bg-[#274CBC] rounded-xl"
-          >
-            Get Started
-          </button>
-          {/* <button className="btn font-bold bg-[#2A2C31] rounded-xl">
-            Find Preexisting Resume
-        </button> */}
-        </section>
-      </Fade>
+    <div className="flex flex-col md:flex-row items-center md:justify-center h-full w-full">
+      <div className="md:flex flex-col md:w-3/7">
+        {/* Title and Description */}
+        <Fade direction="up" duration={1000}>
+          <section className="h-fit mt-20 flex flex-col text-center md:text-start">
+            <h1 className="text-4xl md:text-7xl font-bold mb-2">
+              MAV<span className="font-extralight">RESUME</span>
+            </h1>
+            <p className="text-lg md:text-xl ">
+              Helping undergraduates quickly build tailored, professional
+              resumes - simple, guided, and stress-free.
+            </p>
+          </section>
+        </Fade>
+        {/* Action Buttons */}
+        <Fade direction="up" delay={400} duration={1000}>
+          <section className="flex gap-5 mt-3 justify-center md:justify-start">
+            <button
+              onClick={onContinue}
+              className="btn font-bold bg-[#274CBC] rounded-xl "
+            >
+              Get Started
+            </button>
+            <button className="hidden md:block btn font-bold bg-[#2A2C31] rounded-xl">
+              Find Preexisting Resume
+            </button>
+          </section>
+        </Fade>
+      </div>
 
       {/* Placeholder image of resume mockup */}
       <Fade direction="up" delay={200} duration={1000}>
-        <section className="mt-8">
+        <section className="mt-8 relative w-[30rem] h-[30rem] md:w-[30rem] md:h-[40rem]">
           <Image
             src="/MavResumePlaceholder.png"
             alt="MavResume Group 10"
-            width={300}
-            height={300}
-            className="w-auto h-auto"
+            fill
+            className="object-contain"
           />
         </section>
       </Fade>
